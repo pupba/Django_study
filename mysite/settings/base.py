1ff7b67b8e445e7571de426526aabfaa22e06797
Pupba <rhdk5148@gmail.com>
2023-01-31 19:15:25 +0900

    Django 배포

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -10,23 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_zi2h3%n0mcpd+34in&9w-5a&*=5yt=$o+_7t-a#=t3%(h5n_f'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#SECRET_KEY = 'django-insecure-_zi2h3%n0mcpd+34in&9w-5a&*=5yt=$o+_7t-a#=t3%(h5n_f'
+SECRET_KEY = str(os.environ.get('TEST_SECRET_KEY'))
+# DEBUG가 False 일 떄 적절한 값이 없으면 Django는 동작하지 않음
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
